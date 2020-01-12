5ec54b4f94ad8a06207caaad27466ecf615242a4
Rich Flavell <me@richflavell.com>
2020-01-12 21:57:47 +0000

    fix(build): move css imports to js, add CNAME & bump ver

--- src/config/style/global.ts
+++ src/config/style/global.ts
@@ -20,9 +20,6 @@ const Global = createGlobalStyle`
   }
 
   * {
-    @import url('https://fonts.googleapis.com/css?family=Lato:300,400,300i,400i&display=swap');
-    @import url("https://fonts.googleapis.com/css?family=Montserrat+Alternates:300,400&display=swap");
-    @import url("https://fonts.googleapis.com/icon?family=Material+Icons+Outlined");
     font-family: 'Lato', sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
